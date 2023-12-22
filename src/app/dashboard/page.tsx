9bc82723d11f0bf4e81f4ab2d49f68dff15b71e0
kev0921 <kevin.hu0921@gmail.com>
2023-12-22 14:46:26 -0500

    set up the basic foundation for the app

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import Button from '../Components/ui/Button'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button>hello</Button>
+}
+
+export default page
---
+++ src/app/lib/utils.ts
@@ -0,0 +1,8 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
+// everytime we create conditional classes throughout our application this is the function we are going to use
